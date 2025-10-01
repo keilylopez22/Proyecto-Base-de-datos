b5d80afcee38ec2223aaf06cda0fddf5467046d3
Chrislopz <clopezl45@miumg.edu.gt>
2025-09-30 21:34:51 -0600

    Correccion errores

--- QueryVehiculos.sql
+++ QueryVehiculos.sql
@@ -1,6 +1,8 @@
 CREATE TABLE Garita (
 IdGarita INT PRIMARY KEY,
 NombreGarita VARCHAR (50),
+IdCluster INT,
+IdRegistroVehiculo INT,
 --FOREIGN KEY (IdCluster) REFERENCES  Cluster(IdCluster),
 --FOREIGN KEY (IdRegistroVehiculo) REFERENCES  RegistroVehiculos(IdRegistroVehiculo)
 );
@@ -9,6 +11,10 @@ CREATE TABLE Vehiculo (
 IdVehiculo INT PRIMARY KEY,
 Placa VARCHAR(10),
 Modelo VARCHAR(30),
+IdLinea INT,
+IdMarca INT,
+IdResidente INT,
+IdVisitante INT,
 --FOREIGN KEY (IdLinea) REFERENCES  Linea(IdLinea),
 --FOREIGN KEY (IdMarca) REFERENCES  Marca(IdMarca),
 --FOREIGN KEY (IdResidente) REFERENCES  Residente(IdResidente),
@@ -18,6 +24,7 @@ Modelo VARCHAR(30),
 CREATE TABLE Linea(
 IdLinea INT PRIMARY KEY,
 Descripcion VARCHAR (50),
+IdMarca INT,
 --FOREIGN KEY (IdMarca) REFERENCES  Marca(IdMarca)
 );
 
@@ -26,14 +33,10 @@ IdMarca INT PRIMARY KEY,
 Descripcion VARCHAR(50)
 )
 
-CREATE TABLE Residente (
-IdResidente INT PRIMARY KEY,
---FOREIGN KEY (IdPersona) REFERENCES  Persona(IdPersona),
---FOREIGN KEY (IdVivienda) REFERENCES  Vivienda(IdVivienda)
-);
-
 CREATE TABLE Visitante (
 IdVisitante INT PRIMARY KEY,
+IdPersona INT,
+IdVivienda INT,
 --FOREIGN KEY (IdPersona) REFERENCES  Persona(IdPersona),
 --FOREIGN KEY (IdVivienda) REFERENCES  Vivienda(IdVivienda)
 );
@@ -43,6 +46,9 @@ IdRegistroVehiculo INT PRIMARY KEY,
 FechaHoraEntrada DATE,
 FechaHoraSalida DATE,
 Observaciones VARCHAR(50),
+IdVehiculo INT,
+IdVisitante INT,
+IdResidente INT,
 --FOREIGN KEY (IdVehiculo) REFERENCES  Vehiculo(IdVehiculo),
 --FOREIGN KEY (IdVisitante) REFERENCES  Visitante(IdVisitante),
 --FOREIGN KEY (IdResidente) REFERENCES  Residente(IdResidente)
@@ -52,7 +58,8 @@ CREATE TABLE ListaNegra (
 IdListaNegra INT PRIMARY KEY,
 Causa VARCHAR (100),
 FechaDeclaradoNoGrato DATE,
+IdVehiculo INT,
+IdVisitante INT,
 --FOREIGN KEY (IdVehiculo) REFERENCES  Vehiculo(IdVehiculo),
 --FOREIGN KEY (IdVistante) REFERENCES  Visitante(IdVistante)
-
 );
