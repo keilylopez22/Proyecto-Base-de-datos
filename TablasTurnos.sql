cc2fbf1185ca13ce95bf10f82932032b91a40d62
cchamom <cchamom2@miumg.edu.gt>
2025-09-30 21:41:51 -0600

    Correcciones de PK y FK

--- TablasTurnos.sql
+++ TablasTurnos.sql
@@ -1,59 +1,59 @@
 CREATE TABLE Empleado (
-EmpleadoID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-PersonaID INT NOT NULL,
+IdEmpleado INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+IdPersona INT NOT NULL,
 --FOREIGN KEY (IdPersona) REFERENCES Persona(IdPersona)
 );
 
 CREATE TABLE PuestoTrabajo (
-CodigoPuestoTrabajo INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-Descripcion VARCHAR(50) NOT NULL UNIQUE
+IdPuestoTrabajo INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Descripcion VARCHAR(50) NOT NULL UNIQUEs
 );
 
 CREATE TABLE ContratoTrabajo (
-NumeroContrato INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+IdNumeroContrato INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 FechaEmisionContrato DATE NOT NULL,
 Estado VARCHAR(20) CHECK (Estado IN ('Activo', 'Inactivo', 'Finalizado', 'Suspendido')),
-IdPersona INT NOT NULL,
-CodigoPuestoTrabajo INT NOT NULL,
---FOREIGN KEY (IdPersona) REFERENCES Empleado(IdPersona),
---FOREIGN KEY (CodigoPuestoTrabajo) REFERENCES PuestoTrabajo(CodigoPuestoTrabajo)
+IdEmpleado INT NOT NULL,
+IdPuestoTrabajo INT NOT NULL,
+--FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado),
+--FOREIGN KEY (IdPuestoTrabajo) REFERENCES PuestoTrabajo(IdPuestoTrabajo)
 );
 
 CREATE TABLE Turno (
-TurnoID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+IdTurno INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 Descripcion VARCHAR(50) NOT NULL,
 HoraInicio TIME NOT NULL,
 HoraFin TIME NOT NULL,
 DuracionTurno CHECK(DATEDIFF(HOURS, HoraInicio, HoraFin) <= 24),
-CodigoPuestoTrabajo INT NOT NULL,
---FOREIGN KEY (CodigoPuestoTrabajo) REFERENCES PuestoTrabajo(CodigoPuestoTrabajo)
+IdPuestoTrabajo INT NOT NULL,
+--FOREIGN KEY (IdPuestoTrabajo) REFERENCES PuestoTrabajo(IdPuestoTrabajo)
 );
 
 CREATE TABLE AsignacionTurno (
-AsignacionTurnoID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+IdAsignacionTurno INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 FechaAsignacion DATE NOT NULL,
-EmpleadoID INT NOT NULL,
-TurnoID INT NOT NULL,
+IdEmpleado INT NOT NULL,
+IdTurno INT NOT NULL,
 UNIQUE(EmpleadoID, TurnoID, FechaAsignacion)
---FOREIGN KEY (IdPersona) REFERENCES Empleado(IdPersona),
---FOREIGN KEY (TurnoID) REFERENCES Turno(TurnoID), 
---UNIQUE (IdPersona, TurnoID, FechaAsignacion)
+--FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado),
+--FOREIGN KEY (IdTurno) REFERENCES Turno(IdTurno), 
+--UNIQUE (IdEmpleado, IdTurno, FechaAsignacion)
 );
 
 CREATE TABLE Rondin (
-RondinID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+IdRondin INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 FechaInico DATE NOT NULL,
 FechaFin DATE NOT NULL,
-IdPersona INT,
---FOREIGN KEY (IdPersona) REFERENCES Empleado(EmpleadoID)
+IdEmpleado INT,
+--FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado)
 );
 
 CREATE TABLE DetalleDelRondin (
-DetalleDelRondinID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+IdDetalleDelRondin INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 Hora DATE NOT NULL,
 Lugar VARCHAR(50),
-RondinID INT,
---FOREIGN KEY (RondinID) Rondin Empleado(RondinID)
+IdRondin INT,
+--FOREIGN KEY (IdRondin) Rondin Empleado(IdRondin)
 );
 
 CREATE TABLE RegistroPersonasVisitantes (
@@ -62,6 +62,6 @@ TipoRegistro VARCHAR(50) NOT NULL CHECK(TipoRegistro IN ('Entrada', 'Salida')),
 FechaRegistro TIME NOT NULL,
 TipoDocumento  VARCHAR(50) NOT NULL CHECK(TipoDocumento IN ('DPI', 'Licencia')), 
 NumeroDocumento VARCHAR(50) UNIQUE,
-VisitanteID INT
---FOREIGN KEY (VisitanteID) REFERENCES Visitante(VisitanteID)
+IdVisitante INT
+--FOREIGN KEY (IdVisitante) REFERENCES Visitante(IdVisitante)
 );
