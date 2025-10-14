bd4b20786bb5fbf35483f7ba3e65f105e6d67bd8
cchamom <cchamom2@miumg.edu.gt>
2025-10-13 21:47:00 -0600

    Correcciones a SP

--- STORE PROCEDURE CHAMO/STORE PROCEDURE PERSONA_NO_GRATA/SP_InsertarPersonNoGrata.sql
+++ STORE PROCEDURE CHAMO/STORE PROCEDURE PERSONA_NO_GRATA/SP_InsertarPersonNoGrata.sql
@@ -1,6 +1,6 @@
 CREATE OR ALTER PROCEDURE SP_InsertarPersonNoGrata
 @FechaInicio DATE,
-@FechFin DATE,
+@FechFin DATE = NULL,
 @Motivo VARCHAR(50),
 @IdPersona INT
 AS
@@ -9,13 +9,13 @@ BEGIN
 SET NOCOUNT ON
 IF NOT EXISTS(SELECT 1 FROM Persona WHERE IdPersona = @IdPersona)
 BEGIN 
-RAISERROR('La persona igresada no existe',16,1)
+RAISERROR('La persona ingresada no existe',16,1)
 RETURN
 END
 
-IF EXISTS(SELECT 1 FROM PersonaNoGrata WHERE IdPersona = @IdPersona)
+IF EXISTS(SELECT 1 FROM PersonaNoGrata WHERE IdPersona = @IdPersona AND (FechaFin IS NULL OR FechaFin >= CAST(GETDATE() AS DATE)))
 BEGIN 
-RAISERROR('La perosna que intentas ingresar ya existe en esta lista',16,1)
+RAISERROR('La persona que intentas ingresar ya existe en esta lista y aun no puede ingresar de nuevo hasta que cumpla su sancion',16,1)
 RETURN
 END
 
@@ -26,7 +26,7 @@ END
 
 EXEC SP_InsertarPersonNoGrata
 @FechaInicio = '2025-10-12',
-@FechFin = '',
+@FechFin = '2025-10-13',
 @Motivo = 'Robo a casas',
 @IdPersona = 4
 
--- STORE PROCEDURE CHAMO/STORE PROCEDURE VEHICULO_PROHIBIDO/SP_InsertarVehiculoProhibido.sql
+++ STORE PROCEDURE CHAMO/STORE PROCEDURE VEHICULO_PROHIBIDO/SP_InsertarVehiculoProhibido.sql
@@ -1,5 +1,5 @@
 CREATE OR ALTER PROCEDURE SP_InsertarVehiculoProhibido
-@Fecha DATE,
+@Fecha DATE = NULL,
 @Motivo VARCHAR(50),
 @IdVehiculo INT
 AS
@@ -11,9 +11,9 @@ RAISERROR('El vehicuilo que intenta ingresar en la lista de prohibidos no exsite
 RETURN 
 END
 
-IF EXISTS(SELECT 1 FROM VehiculoProhibido WHERE IdVehiculo = @IdVehiculo)
+IF EXISTS(SELECT 1 FROM VehiculoProhibido WHERE IdVehiculo = @IdVehiculo AND (Fecha IS NULL OR FECHA >= CAST(GETDATE() AS DATE)))
 BEGIN
-RAISERROR('El vehiculo ya se encuentra prohibido',16,1)
+RAISERROR('El vehiculo ya se encuentra prohibido y no puede ser prohibido de nuevo  hasta que cumpla su sancion',16,1)
 RETURN
 END
 
