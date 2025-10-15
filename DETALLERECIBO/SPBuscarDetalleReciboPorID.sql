1cb21827e7bf878baa705d620931abcdfb20b13a
Delmy <dfajardob@miumg.gt>
2025-10-14 22:24:26 -0600

    correccion
    agregue el nombre de la persona

--- DETALLERECIBO/SPBuscarDetalleReciboPorID.sql
+++ DETALLERECIBO/SPBuscarDetalleReciboPorID.sql
@@ -3,10 +3,14 @@ CREATE OR ALTER PROCEDURE BuscarDetalleReciboPorID
 @IdDetalleRecibo INT 
 AS
 BEGIN
-	SELECT *
-	From DetalleRecibo
+	SELECT dr.IdRecibo,csv.MontoAplicado,v.NumeroVivienda,v.IdCluster, per.PrimerNombre, per.SegundoNombre, per.PrimerApellido, per.SegundoApellido
+	From DetalleRecibo AS dr
+	INNER JOIN CobroServicioVivienda AS csv ON dr.idCobroServicio = csv.idCobroServicio
+	INNER JOIN Vivienda AS v ON csv.NumeroVivienda = v.NumeroVivienda 
+	INNER JOIN Residente AS r ON  v.NumeroVivienda = r.NumeroVivienda
+	INNER JOIN Persona AS per ON r.IdPersona = per.IdPersona
 	WHERE IdDetalleRecibo = @IdDetalleRecibo
 END;
 GO
 EXEC BuscarDetalleReciboPorID
-@IdDetalleRecibo = 1
+@IdDetalleRecibo = 3
