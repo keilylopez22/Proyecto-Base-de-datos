6a2b88492849cc914c28aee0838966c49da8d043
cchamom <cchamom2@miumg.edu.gt>
2025-10-16 20:45:59 -0600

    Refatcorizar

--- STORE PROCEDURE CHAMO/STORE PROCEDURE ASIGNACION_TURNOS/SP_CrearAsignacionTurno.sql
+++ STORE PROCEDURE CHAMO/STORE PROCEDURE ASIGNACION_TURNOS/SP_CrearAsignacionTurno.sql
@@ -12,17 +12,12 @@ RAISERROR('No existe este empleado',16,1)
 RETURN
 END
 
-IF EXISTS(SELECT 1 FROM AsignacionTurno WHERE IdEmpleado = @IdEmpleado AND FechaAsignacion = @FechaAsignacion)
-BEGIN
-RAISERROR('El empleado ya cuenta con un turno asignado',16,1)
-RETURN
-END
 
 INSERT INTO AsignacionTurno(FechaAsignacion, IdEmpleado, IdTurno)
 VALUES(@FechaAsignacion,@IdEmpleado, @IdTurno)
 END
 
 EXEC SP_CrearAsignacionTurno
-@FechaAsignacion = '2025-10-02',
-@IdEmpleado = 6,
-@IdTurno = 1
+@FechaAsignacion = '2025-10-15',
+@IdEmpleado = 1,
+@IdTurno = 3
