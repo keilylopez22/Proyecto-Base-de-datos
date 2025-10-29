d68ff9fb1e2db955bc6596ec1e97d72c9dce2b42
Delmy <dfajardob@miumg.gt>
2025-10-28 22:33:56 -0600

    personas a propietario
    agregacion de me metodo de asignarPropietario y boton

--- ArsanFWebApp/Controllers/PersonaController.cs
+++ ArsanFWebApp/Controllers/PersonaController.cs
@@ -1,16 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using ArsanWebApp.Models;
 using ArsanWebApp.Services;
+using System.Threading.Tasks;
 
 namespace ArsanFWebApp.Controllers;
 
 public class PersonaController : Controller
 {
     private readonly PersonaService _service;
+    private readonly PropietarioService _propietarioService;
 
-    public PersonaController(PersonaService service)
+    public PersonaController(PersonaService service, PropietarioService propietarioService)
     {
         _service = service;
+        _propietarioService = propietarioService;
     }
 
 
@@ -86,5 +89,16 @@ public class PersonaController : Controller
     {
         await _service.EliminarAsync(id);
         return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> AsignarPropietario(int id)
+    {
+        bool yaEsPropietario = await _propietarioService.ExistePropietarioPorPersonaId(id);
+        if (yaEsPropietario)
+        {
+            TempData["ErrorMessage"] = "La persona ya es un propietario.";
+            return RedirectToAction(nameof(Index));
+        }
+        return RedirectToAction("Create", "Propietario", new { idPersona = id });
     }
 }
